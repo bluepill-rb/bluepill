45884fc9d30bf2dd13f1f00f3d0b1b979242a304
Arya Asemanfar <arya.asemanfar@gmail.com>
2009-10-03 14:20:36 -0700

    condition watch improvements

--- lib/bluepill/condition_watch.rb
+++ lib/bluepill/condition_watch.rb
@@ -1,42 +1,40 @@
 module BluePill
   class ConditionWatch
+    attr_accessor :logger
     def initialize(name, options = {})
       @name = name
-      
+
+      @logger = options.delete(:logger)      
       @fires = options.has_key?(:fires) ? [options.delete(:fires)].flatten : [:restart]
       @every = options.delete(:every)
-      @times = options.delete(:times)
-      
-      @last_ran_at = nil
-      @histroy = []
+      @times = options.delete(:times) || [1,1]
+      @times = [@times, @times] unless @times.is_a?(Array) # handles :times => 5
+
+      self.clear_history!
       
       @process_condition = ProcessConditions.name_to_class(@name).new(options)
     end
     
-    def run(pid)
-      if @last_ran_at.nil? || (@last_ran_at + @every) >= Time.now.to_i
-        @last_ran_at = Time.now_to_i
+    def run(pid, tick_number = Time.now.to_i)
+      if @last_ran_at.nil? || (@last_ran_at + @every) >= tick_number
+        @last_ran_at = tick_number
         self.record_value(@process_condition.run(pid))
         return @fires if self.fired?
       end
-      return []
+      []
     end
     
     def record_value(value)
       # TODO: record value in ProcessStatistics
-      unless @times.nil?
-        if @times.is_a?(Array)
-          @history.push([value, @process_condition.check(value)])
-          @history.shift if @history.size > @times[1]
-        else
-          @history = [value]
-        end
-      end
+      logger.info(self.to_s)
+      @history[@history_index] = [value, @process_condition.check(value)]
+      @history_index = (@history_index + 1) % @history.size
     end
     
     def clear_history!
-      @history = []
       @last_ran_at = nil
+      @history = Array.new(@times[1])
+      @history_index = 0
     end
     
     def fired?
@@ -44,6 +42,10 @@ module BluePill
     end
     
     def to_s
+      # TODO: this will be out of order because of the way history values are assigned
+      # use (@history[(@history_index - 1)..1] + @history[0..(@history_index - 1)]).
+      #       collect {|v| "#{v[0]}#{v[1] ? '' : '*'}"}.join(", ")
+      # but that's gross so... it's gonna be out of order till we figure out a better way to get it in order
       @history.collect {|v| "#{v[0]}#{v[1] ? '' : '*'}"}.join(", ")
     end
   end

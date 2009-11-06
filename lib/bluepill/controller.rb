3d833ed80ddb32a750270c969f60bcfcb1af0132
Arya Asemanfar <arya.asemanfar@gmail.com>
2009-11-06 10:12:18 -0800

    error message for when you need to specify a sock file or there are no sock files

--- lib/bluepill/controller.rb
+++ lib/bluepill/controller.rb
@@ -11,10 +11,23 @@ module Bluepill
       self.pids_dir = File.join(base_dir, 'pids')
       self.applications = Hash.new 
       setup_dir_structure
+      cleanup
     end
     
     def list
       Dir[File.join(sockets_dir, "*.sock")].map{|x| File.basename(x, ".sock")}
+    end
+    
+    def cleanup
+      self.list.each do |app|
+        pid = pid_for(app)
+        if !pid || !alive?(pid)
+          pid_file = File.join(self.pids_dir, "#{app}.pid")
+          sock_file = File.join(self.sockets_dir, "#{app}.sock")
+          File.unlink(pid_file) if File.exists?(pid_file)
+          File.unlink(sock_file) if File.exists?(sock_file)
+        end
+      end
     end
     
     def send_cmd(application, command, *args)
@@ -23,6 +36,21 @@ module Bluepill
     end
     
     private
+    
+    def pid_for(app)
+      pid_file = File.join(self.pids_dir, "#{app}.pid")
+      File.exists?(pid_file) && File.read(pid_file).to_i
+    end
+    
+    
+    def alive?(pid)
+      begin
+        ::Process.kill(0, pid)
+        true
+      rescue Errno::ESRCH
+        false
+      end
+    end
     
     def setup_dir_structure
       [@sockets_dir, @pids_dir].each do |dir|

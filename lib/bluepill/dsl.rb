d81e4881f34d2f0351ef78e717465fd6473421b5
Arya Asemanfar <arya.asemanfar@gmail.com>
2009-11-22 15:47:34 -0800

    added a few validations for config

--- lib/bluepill/dsl.rb
+++ lib/bluepill/dsl.rb
@@ -5,7 +5,8 @@ module Bluepill
    
     process_proxy = Class.new do
       attr_reader :attributes, :watches
-      def initialize
+      def initialize(process_name = nil)
+        @name = process_name
         @attributes = {}
         @watches = {}
       end
@@ -24,6 +25,14 @@ module Bluepill
         @watches[name] = options
       end
       
+      def validate_child_process(child)
+        unless child.attributes.has_key?(:stop_command)
+          $stderr.puts "Config Error: Invalid child process monitor for #{@name}"
+          $stderr.puts "You must specify a stop command to monitor child processes."
+          exit(6)
+        end
+      end
+      
       def monitor_children(&child_process_block)
         child_proxy = self.class.new
         
@@ -33,6 +42,7 @@ module Bluepill
         child_proxy.restart_grace_time = @attributes[:restart_grace_time]
         
         child_process_block.call(child_proxy)
+        validate_child_process(child_proxy)
         
         @attributes[:child_process_template] = child_proxy.to_process(nil)
         # @attributes[:child_process_template].freeze
@@ -55,10 +65,27 @@ module Bluepill
     app_proxy = Class.new do
       @@app = app
       @@process_proxy = process_proxy
+      @@process_names = Hash.new # becuase I don't want to require Set just for validations
+      def validate_process(process, process_name)
+        if @@process_names.key?(process_name)
+          $stderr.puts "Config Error: You have two entries for the process name '#{process_name}'"
+          exit(6)
+        else
+          @@process_names[process_name] = 0
+        end
+        
+        [:start_command, :pid_file].each do |required_attr|
+          if !process.attributes.key?(required_attr)
+            $stderr.puts "Config Error: You must specify a #{required_attr} for '#{process_name}'"
+            exit(6)
+          end
+        end
+      end
       
       def process(process_name, &process_block)
-        process_proxy = @@process_proxy.new
+        process_proxy = @@process_proxy.new(process_name)
         process_block.call(process_proxy)
+        validate_process(process_proxy, process_name)
         
         group = process_proxy.attributes.delete(:group)        
         process = process_proxy.to_process(process_name)

b395d54f6f521fc820a0218bbe6b20a917559fbd
Barry Allard <barry.allard@gmail.com>
2013-03-17 01:40:19 -0700

    add process resource accounting feature :include_children

--- lib/bluepill/system.rb
+++ lib/bluepill/system.rb
@@ -26,12 +26,24 @@ module Bluepill
       end
     end
 
-    def cpu_usage(pid)
-      ps_axu[pid] && ps_axu[pid][IDX_MAP[:pcpu]].to_f
+    def cpu_usage(pid, include_children)
+      ps = ps_axu
+      return unless ps[pid]
+      cpu_used = ps[pid][IDX_MAP[:pcpu]].to_f
+      get_children(pid).each { |child_pid|
+        cpu_used += ps[child_pid][IDX_MAP[:pcpu]].to_f if ps[child_pid]
+      } if include_children
+      cpu_used 
     end
 
-    def memory_usage(pid)
-      ps_axu[pid] && ps_axu[pid][IDX_MAP[:rss]].to_f
+    def memory_usage(pid, include_children)
+      ps = ps_axu
+      return unless ps[pid]
+      mem_used = ps[pid][IDX_MAP[:rss]].to_f
+      get_children(pid).each { |child_pid|
+        mem_used += ps[child_pid][IDX_MAP[:pcpu]].to_f if ps[child_pid]
+      } if include_children
+      mem_used
     end
 
     def get_children(parent_pid)
@@ -39,7 +51,8 @@ module Bluepill
       ps_axu.each_pair do |pid, chunks|
         child_pids << chunks[IDX_MAP[:pid]].to_i if chunks[IDX_MAP[:ppid]].to_i == parent_pid.to_i
       end
-      child_pids
+      grand_children = child_pids.map{|pid| get_children(pid)}.flatten
+      child_pids.concat grand_children 
     end
 
     # Returns the pid of the child that executes the cmd
